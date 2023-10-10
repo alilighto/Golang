be006eb0cb8b89171d7449e6e5a3a6a5b41e03b8
alilighto <alilighto02@gmail.com>
2023-10-10 14:03:07 +0100

    golang art

---
+++ Gopher.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main ()
+{
+fmt.Println("    `.-::::::-.`    ")
+fmt.Println(".:-::::::::::::::-:.")
+fmt.Println("`_:::    ::    :::_`")
+fmt.Println(" .:( ^   :: ^   ):. ")
+fmt.Println(" .:::   (..)   :::. ")
+fmt.Println(" `:::::::UU:::::::` ")
+fmt.Println(" .::::::::::::::::. ")
+fmt.Println(" O::::::::::::::::O ")
+fmt.Println(" -::::::::::::::::- ")
+fmt.Println(" `::::::::::::::::` ")
+fmt.Println("  .::::::::::::::.  ")
+fmt.Println("    oO::::::::Oo    ")
+}
